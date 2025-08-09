ddf8a8fbdc151446d8dbc1b0e3d3d46ba2cb27bb
pyke <pykelysia0206@163.com>
2025-08-09 15:28:00 +0800

    refactor(path): to_current_path

--- path.py
+++ path.py
@@ -1,11 +1,14 @@
 import subprocess
 import os
 
-# 前往指定的 git 位置
-# return
-# - False -> 出现错误
-# - True  -> 进入成功
+
 def to_current_path(path: str) -> bool:
+    """
+    前往指定的 git 位置
+    return:
+    - False -> 出现错误
+    - True  -> 进入成功
+    """
     if not os.path.exists(path):
         print("path not exists")
         return False
