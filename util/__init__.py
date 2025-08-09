1112274ba416f7863ee4afc3aa2f9bfdb3328523
pyke <pykelysia0206@163.com>
2025-08-09 15:28:36 +0800

    feat(util): 增加打印函数
    - 在 __print__.py 中添加了 print_start、print_end、print_warning 和 print_info 函数
    - 更新了 __init__.py 以导入新增的打印函数

--- util/__init__.py
+++ util/__init__.py
@@ -1,1 +1,3 @@
 from .__print__ import print_error, print_success
+from .__print__ import print_start, print_end
+from .__print__ import print_warning, print_info
--- util/__print__.py
+++ util/__print__.py
@@ -4,6 +4,22 @@ def print_success(message: str):
 def print_error(message: str):
     print(f"[error]   {message}")
 
+def print_start(message: str):
+    print(f"[start]   {message}")
+
+def print_end(message: str):
+    print(f"[end]     {message}")
+
+def print_warning(message: str):
+    print(f"[warning] {message}")
+
+def print_info(message: str):
+    print(f"[info]    {message}")
+
 if __name__ == "__main__":
     print_success("Hello World")
     print_error("Error")
+    print_start("Start")
+    print_end("End")
+    print_warning("Warning")
+    print_info("Info")
