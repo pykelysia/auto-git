cd9ec9374520f815bae1811c0c27caadb820a52c
pyke <pykelysia0206@163.com>
2025-10-26 21:52:06 +0800

    feat(run_with_cmd): 添加命令行读取

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import getopt
+import sys
 import path
 import mode
 import util
+from path import to_current_path
 
+shortopts = 'dp'
+longopts = ['default', 'path']
 
 def main():
+    if run_with_cmd():
+        return
+
     # 前往正确的路径
     while True:
         path_str = input("path of git repository: ")
@@ -16,6 +24,17 @@ def main():
     # 默认
     if mode_select == "":
         mode.default_mode()
+
+def run_with_cmd()->bool:
+    opts, args = getopt.getopt(sys.argv[1:], shortopts, longopts)
+    for o, a in opts:
+        if o in ('-p', '--path'):
+            to_current_path(a)
+        if o in ('-d', '--default'):
+            mode.default_mode()
+            return True
+    return False
+
 
 if __name__ == '__main__':
     util.print_start("Starting...")
